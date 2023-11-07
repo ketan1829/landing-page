7d951297c48f9f21bed17683acce973bee8151ef
kchoira <ketan.salvi21@gmail.com>
2023-11-07 20:33:23 +0530

    debug

--- types.ts
+++ types.ts
@@ -19,3 +19,4 @@ export type NonNullableChildren<T> = { [P in keyof T]: Required<NonNullable<T[P]
 export type NonNullableChildrenDeep<T> = {
   [P in keyof T]-?: NonNullableChildrenDeep<NonNullable<T[P]>>;
 };
+
